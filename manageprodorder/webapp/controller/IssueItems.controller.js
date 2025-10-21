f182de94cdb49d1af54749051904af79b7f1c911
shankdev25 <shashank5114001@gmail.com>
2025-10-21 18:13:50 +0300

    Messagebox fix

--- manageprodorder/webapp/controller/IssueItems.controller.js
+++ manageprodorder/webapp/controller/IssueItems.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox"
+], function (Controller, JSONModel,MessageBox) {
     "use strict";
     return Controller.extend("com.merkavim.ewm.manageprodorder.controller.IssueItems", {
         onNavBack: function () {
